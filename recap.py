aa027a496672d5d4d743e49a03e32dd199712b93
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2021-08-02 19:41:31 +0200

    Update recap.py

--- recap.py
+++ recap.py
@@ -139,6 +139,81 @@ class Modele(object):
         test = self.myList.count()
         print(test)
 
+class Activated(object):
+    """
+    La fonction activated() permet d'activer automatiquement les données
+    d'un widget sans recourir à un bouton d'exécution
+    Fonction retrouvée dans les cours suivants dans codemy_pyqt5:
+    index023_ComboBoxes
+    """
+    
+    def setupUi(self, MainWindow):
+        "Configuration des widgets"
+        
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(829, 262)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        
+        # Titre
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(120, 30, 661, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        
+        # Menu déroulant
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(120, 110, 441, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.comboBox.setFont(font)
+        self.comboBox.setObjectName("comboBox")
+        
+        # Faire du menu déroulant un widget cliquable
+        self.comboBox.activated.connect(self.clicker)
+        
+        # Ajout des données dans le menu déroulant
+        self.comboBox.addItem('Calzone')
+        self.comboBox.addItem('Champignons')
+        self.comboBox.addItem('Fromages')
+        self.comboBox.addItem('Poivrons')
+        
+        # Ajout d'autres données par une liste
+        my_toppings = ['Jambon', 'Ananas', 'Suppreme']
+        self.comboBox.addItems(my_toppings)
+                
+        # Barre de menus
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 829, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        
+        # Barre de statuts
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        "Noms rattachés aux widgets"
+        
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate(
+            "MainWindow", 
+            "Sélectionner un élément dans le menu déroulant"))
+        
+    def clicker(self):
+        "Changement du titre selon la pizza selectionnée"
+        
+        self.label.setText(
+            f"Vous avez choisi : {self.comboBox.currentText()}")
+
 class AddItem(object):
     """
     La fonction addItem() permet d'ajouter des données dans un widget
@@ -849,6 +924,7 @@ class SetText(object):
     index016_linkButton
     index018_radioButtons
     index019_radioButtons2
+    index023_ComboBoxes
     """
 
     def setupUi(self, MainWindow):
@@ -1190,6 +1266,8 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     """Liste ci-après des fonctions natives de pyqt5"""
+    # activated = Activated()
+    # activated.setupUi(MainWindow)
     # addItem = AddItem()
     # addItem.setupUi(MainWindow)
     # clear = Clear()
